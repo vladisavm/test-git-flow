f02f713beefde19cc79ea3f9dd223754cd157572
Vladisav Milosavljevic <vladisav.milosavljevic@teliasonera.com>
2017-03-13 12:08:06 +0100

    Updated source

--- src/main/java/test/git/flow/App.java
+++ src/main/java/test/git/flow/App.java
@@ -4,10 +4,10 @@ package test.git.flow;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(final String[] args) {
+
+        System.out.println("Hello World, Git Flow!");
+
     }
 }
