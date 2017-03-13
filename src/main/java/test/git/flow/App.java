1c3ad0c34afac4c763929c4e3f38bc1f82507a76
Vladisav Milosavljevic <vladisav.milosavljevic@teliasonera.com>
2017-03-13 14:01:35 +0100

    Updated msgs

--- src/main/java/test/git/flow/App.java
+++ src/main/java/test/git/flow/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main(final String[] args) {
 
         System.out.println("Hello World, Git Flow!");
+        System.out.println("Hello mr. Oz");
 
     }
 }
